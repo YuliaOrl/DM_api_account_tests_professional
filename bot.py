a7e9bcae196eb3f1f9c04e736e58a72089427813
Yulia <orbitalik32@gmail.com>
2026-06-27 15:31:03 +0300

    fix path для расположения bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import TeleBot
 from vyper import v
 
 
-config = Path(__file__).parent.joinpath("../../").joinpath("config")
+config = Path(__file__).parent.joinpath("config")
 v.set_config_name("stg")
 v.add_config_path(config)
 v.read_in_config()
